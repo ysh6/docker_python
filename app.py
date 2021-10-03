8a99c15c263d9ae9ebcd1f9666827f45c5ae9245
ysh6 <78426518+ysh6@users.noreply.github.com>
2021-10-03 09:47:56 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
-import os
-import socket
-app = Flask(__name__)@app.route("/")def hello():
-     html = "<h3>Hello {name}!</h3>" \
-            "<b>Hostname:</b> {hostname}<br/>"
-     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
-if __name__ == "__main__":
-app.run(host=’0.0.0.0', port=80)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Web App with Python Flask!'
+
+app.run(host='0.0.0.0', port=80)
